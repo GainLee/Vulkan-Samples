a0a9c59aff2a1676278a6efad280d4b9ca1dfadd
GainLee <perfecter.gen@gmail.com>
2022-07-21 11:39:18 +0800

    Add triangle demo with framework

--- samples/camera/camera_preview/camera_preview.cpp
+++ samples/camera/camera_preview/camera_preview.cpp
@@ -39,8 +39,8 @@ bool CameraPreview::prepare(vkb::Platform &platform)
 	// Example Scene Render Pipeline
 	vkb::ShaderSource vert_shader("triangle.vert");
 	vkb::ShaderSource frag_shader("triangle.frag");
-	auto              triangle_subpass   = std::make_unique<TriangleSubpass>(get_render_context(), std::move(vert_shader), std::move(frag_shader));
-	auto              render_pipeline = vkb::RenderPipeline();
+	auto              triangle_subpass = std::make_unique<TriangleSubpass>(get_render_context(), std::move(vert_shader), std::move(frag_shader));
+	auto              render_pipeline  = vkb::RenderPipeline();
 	render_pipeline.add_subpass(std::move(triangle_subpass));
 	set_render_pipeline(std::move(render_pipeline));
 
@@ -57,29 +57,25 @@ std::unique_ptr<vkb::VulkanSample> create_camera_preview()
 }
 
 CameraPreview::TriangleSubpass::TriangleSubpass(vkb::RenderContext &render_context, vkb::ShaderSource &&vertex_shader, vkb::ShaderSource &&fragment_shader) :
-        vkb::Subpass(render_context, std::move(vertex_shader), std::move(fragment_shader))
+    vkb::Subpass(render_context, std::move(vertex_shader), std::move(fragment_shader))
 {
 }
 
 void CameraPreview::TriangleSubpass::prepare()
 {
-    auto &device   = render_context.get_device();
-    auto &vertex   = device.get_resource_cache().request_shader_module(VK_SHADER_STAGE_VERTEX_BIT, get_vertex_shader());
-    auto &fragment = device.get_resource_cache().request_shader_module(VK_SHADER_STAGE_FRAGMENT_BIT, get_fragment_shader());
-    layout         = &device.get_resource_cache().request_pipeline_layout({&vertex, &fragment});
+	auto &device   = render_context.get_device();
+	auto &vertex   = device.get_resource_cache().request_shader_module(VK_SHADER_STAGE_VERTEX_BIT, get_vertex_shader());
+	auto &fragment = device.get_resource_cache().request_shader_module(VK_SHADER_STAGE_FRAGMENT_BIT, get_fragment_shader());
+	layout         = &device.get_resource_cache().request_pipeline_layout({&vertex, &fragment});
 }
 
 void CameraPreview::TriangleSubpass::draw(vkb::CommandBuffer &command_buffer)
 {
-    command_buffer.bind_pipeline_layout(*layout);
+	command_buffer.bind_pipeline_layout(*layout);
 
-    // A depth-stencil attachment exists in the default render pass, make sure we ignore it.
-    /*vkb::DepthStencilState ds_state = {};
-    ds_state.depth_test_enable      = VK_FALSE;
-    ds_state.stencil_test_enable    = VK_FALSE;
-    ds_state.depth_write_enable     = VK_FALSE;
-    ds_state.depth_compare_op       = VK_COMPARE_OP_ALWAYS;
-    command_buffer.set_depth_stencil_state(ds_state);*/
+	vkb::RasterizationState rasterization_state{};
+	rasterization_state.cull_mode = VK_CULL_MODE_NONE;
+	command_buffer.set_rasterization_state(rasterization_state);
 
-    command_buffer.draw(3, 1, 0, 0);
+	command_buffer.draw(3, 1, 0, 0);
 }
